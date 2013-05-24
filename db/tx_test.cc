648dd2bae5ff05911e98be64fe2915ace30c4e94
qh <qh@R910.R910>
2013-05-24 15:53:54 +0800

    tx equal test

--- db/tx_test.cc
+++ db/tx_test.cc
@@ -4,6 +4,11 @@
 
 #include "db/dbtransaction.h"
 #include "leveldb/env.h"
+#include "port/port.h"
+#include "util/mutexlock.h"
+
+static int FLAGS_threads = 1;
+
 namespace leveldb {
 /*
 class Transaction {
@@ -57,15 +62,16 @@ class Benchmark {
 		mutex = m;
 	}
 	struct ThreadArg {
-		ThreadState *tid;
+		ThreadState *thread;
 		HashTable *seqs;
 		MemTable *store;
 		port::Mutex *mutex;
+		
 	};
 	static void ThreadBody(void* v) {
 		ThreadArg* arg = reinterpret_cast<ThreadArg*>(v);
-		int tid = (arg->tid)->tid;
-		sharedState *shared = arg->shared;
+		int tid = (arg->thread)->tid;
+		SharedState *shared = arg->thread->shared;
 		HashTable *seqs = arg->seqs;
 		MemTable *store = arg->store;
 		port::Mutex *mutex = arg->mutex;
@@ -80,7 +86,7 @@ class Benchmark {
 		for (int i=tid*10; i< (tid+1)*10; i++ ) {
 			DBTransaction tx(seqs, store, mutex);
 			bool b = false;
-			while (!b) {
+			while (b==false) {
 			tx.Begin();
 			
 			for (int j=1; j<4; j++) {
@@ -97,12 +103,14 @@ class Benchmark {
 				tx.Add(t, k, *v);			
 			}
 			b = tx.End();
+			//if (b==true)printf("%d\n", i);
 			}
 			
 			DBTransaction tx1(seqs, store, mutex);
 			b =false;
-			while (!b) {
+			while (b==false) {
 			tx1.Begin();
+			
 			for (int j=1; j<4; j++) {
 				char* key = new char[100];
 				snprintf(key, sizeof(key), "%d", j);
@@ -110,12 +118,16 @@ class Benchmark {
 				
 
 				Status s;
+				printf("Read begin %d\n",j);
 				tx1.Get(k, &(str[j-1]), &s);
+				printf("Tid %d get %s %s\n",tid,key,&str[j-1]);
 			}						
 			b = tx1.End();
+			if (b==true)printf("%d\n", i);
 			}
 			assert(str[0]==str[1]);
 			assert(str[1]==str[2]);
+			printf("Tid %d Iter %d\n",tid,i);
 
 		}
 		{
@@ -130,7 +142,7 @@ class Benchmark {
 	void Run() {
 		 
 		SharedState shared;
-		shared.total = 4;
+		shared.total = FLAGS_threads;
 		shared.num_initialized = 0;
 		shared.start_time = 0;
 		shared.end_time = 0;
@@ -138,21 +150,21 @@ class Benchmark {
 		shared.start = false;
 		 
 		 
-		 ThreadArg* arg = new ThreadArg[4];
+		 ThreadArg* arg = new ThreadArg[FLAGS_threads];
 		 
-		 for (int i = 0; i < 4; i++) {	 	
-		 	arg[i].tid = new ThreadState(i);
+		 for (int i = 0; i < FLAGS_threads; i++) {	 	
+		 	arg[i].thread = new ThreadState(i);
 			arg[i].seqs = seqs;
 			arg[i].store = store;
 			arg[i].mutex = mutex;
-			arg[i].shared = &shared;
+			arg[i].thread->shared = &shared;
 			//printf("Out tid %lx\n", &arg[i]);
 			Env::Default()->StartThread(ThreadBody, &arg[i]);
 			
 		 }
 
 		 shared.mu.Lock();
-		 while (shared.num_done < n) {
+		 while (shared.num_done < FLAGS_threads) {
 		  shared.cv.Wait();
 		 }
 		 shared.mu.Unlock();
