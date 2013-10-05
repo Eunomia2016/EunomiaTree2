f7dbe460d824e0b668a585d2f01ee090aeaf9669
Zhaoguo Wang <tigerwang1986@gmail.com>
2013-10-05 23:45:38 +0800

    working set done

--- microbench/workingset.cc
+++ microbench/workingset.cc
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <assert.h>
+#include <sched.h>
 #include "rtmRegion.h"
 
 typedef unsigned long uint64_t;
@@ -21,11 +22,12 @@ struct Cacheline {
 //critical data
 char padding[64];
 int workingset = 16 * 1024; //Default ws: 16KB
-Cacheline *array;
+__thread Cacheline *array;
 char padding1[64];
 
 volatile int ready = 0;
 volatile int epoch = 0;
+int thrnum = 1;
 
 int tmpcount;
 inline int Read(char * data) {
@@ -66,6 +68,14 @@ diff_timespec(const struct timespec &end, const struct timespec &start)
     return diff;
 }
 
+void thread_init(){
+	//Allocate the array at heap
+	array = (Cacheline *)malloc(ARRAYSIZE * sizeof(Cacheline));
+		
+	//Touch every byte to avoid page fault 
+	memset(array, 1, ARRAYSIZE * sizeof(Cacheline)); 
+
+}
 
 void* thread_body(void *x) {
 
@@ -74,6 +84,18 @@ void* thread_body(void *x) {
 	int lepoch = 0;
 	
 	struct timespec start, end;
+	
+	uint64_t tid = (uint64_t)x;
+	int cpu = 1;
+	if(tid == 1)
+		cpu = 5;
+
+	cpu_set_t  mask;
+    CPU_ZERO(&mask);
+    CPU_SET(cpu, &mask);
+    sched_setaffinity(0, sizeof(mask), &mask);
+
+	thread_init();
 	
 	__sync_fetch_and_add(&ready, 1);
 	
@@ -87,14 +109,16 @@ void* thread_body(void *x) {
 		
 		{
 			RTMRegion rtm(&prof);
-			count += Read((char *)array);	
+			count += Read((char *)array);
+			//Write((char *)array);
 		}
 
 		if(lepoch < epoch) {
 			clock_gettime(CLOCK_REALTIME, &end);
+			printf("Thread [%d] Time %.2f s\n", 
+						tid, diff_timespec(end, start)/1000.0);
 			prof.ReportProfile();
 			prof.Reset();
-			printf("Time %.2f s\n", diff_timespec(end, start)/1000.0);
 			printf("count %d\n", count);
 			clock_gettime(CLOCK_REALTIME, &start);
 			lepoch = epoch;
@@ -119,20 +143,18 @@ int main(int argc, char** argv) {
 					return 1;
 		}
 		else if(sscanf(argv[i], "--ws=%d%c", &n, &junk) == 1) {
-					workingset = n * 1024;
+			workingset = n * 1024;
+		}
+		else if(strcmp(argv[i], "--ht") == 0) {
+			thrnum = 2;
 		}
 	}
 
 	printf("Touch Work Set %d\n", workingset);
 	
-	//Allocate the array at heap
-	array = (Cacheline *)malloc(ARRAYSIZE * sizeof(Cacheline));
-	
-	//Touch every byte to avoid page fault 
-	memset(array, 1, ARRAYSIZE * sizeof(Cacheline)); 
-	
-	pthread_t th;
-	pthread_create(&th, NULL, thread_body, NULL);
+	pthread_t th[2];
+	for(int i = 0; i < thrnum; i++)
+		pthread_create(&th[i], NULL, thread_body, (void *)i);
 
 	//Barriar to wait all threads become ready
 	while (ready < 1);
@@ -144,7 +166,10 @@ int main(int argc, char** argv) {
 		sleep(5);
 		epoch++;
 	}
-	pthread_join(th, NULL);
+
+	for(int i = 0; i < thrnum; i++)
+		pthread_join(th[i], NULL);
+	
 	
 	return 1;
 }
