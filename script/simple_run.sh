321cd2d3419161c9d558731e87ab465bede48a7c
wangxinalex <wangxinalex@gmail.com>
2016-01-25 16:18:56 +0800

    scale-factor removed

--- script/simple_run.sh
+++ script/simple_run.sh
@@ -6,6 +6,6 @@ fi
 
 num_threads=$1
 m=$(expr 100000 / $num_threads)
-echo "../dbtest --bench tpcc --num-threads $num_threads --ops-per-worker $m --retry-aborted-transactions --scale-factor $num_threads --verbose"
+echo "../dbtest --bench tpcc --num-threads $num_threads --ops-per-worker $m --retry-aborted-transactions --verbose"
 #pin -t $PIN_ROOT/source/tools/ManualExamples/obj-intel64/inscount0.so -- ../dbtest --bench tpcc --num-threads $num_threads --ops-per-worker $m --retry-aborted-transactions --verbose
 ../dbtest --bench tpcc --num-threads $num_threads --ops-per-worker $m --retry-aborted-transactions --verbose
