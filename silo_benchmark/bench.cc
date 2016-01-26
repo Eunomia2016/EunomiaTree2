dd2a97bab42e990fa588d39d279bd2567bcdebc2
wangxinalex <wangxinalex@gmail.com>
2016-01-26 19:53:26 +0800

    local modification

--- silo_benchmark/bench.cc
+++ silo_benchmark/bench.cc
@@ -153,7 +153,9 @@ bench_worker::run() {
 	int socket_0[] = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
 	int mixed_sockets[] = {0, 1, 2, 3, 4, 5, 6, 7, \ 
 		8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+	int cross_sockets[] = {0,2,4,6,8,10,12,14,16,18,1,3,5,7,9,11,13,15,17,19};
 	//int core_id = socket_0[y];
+	//int core_id = cross_sockets[y];
 	int core_id = mixed_sockets[y];
 
 	cpu_set_t mask;
